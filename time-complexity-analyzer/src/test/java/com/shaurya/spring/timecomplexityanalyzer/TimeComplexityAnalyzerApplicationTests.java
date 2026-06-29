398841760d6deaf3fffe78cd01878b2f40ba9b1f
Shaurya shukla <ks9205124@gmail.com>
2026-06-29 13:34:14 +0530

    Initial Spring Boot project setup.

---
+++ time-complexity-analyzer/src/test/java/com/shaurya/spring/timecomplexityanalyzer/TimeComplexityAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaurya.spring.timecomplexityanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeComplexityAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
