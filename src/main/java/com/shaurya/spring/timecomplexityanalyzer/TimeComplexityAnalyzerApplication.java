a324ea77bb17d13178f0c8b767cebe362384d258
Shaurya shukla <ks9205124@gmail.com>
2026-07-09 11:39:33 +0530

    fix: main.java fixed

--- src/main/java/com/shaurya/spring/timecomplexityanalyzer/TimeComplexityAnalyzerApplication.java
+++ src/main/java/com/shaurya/spring/timecomplexityanalyzer/TimeComplexityAnalyzerApplication.java
@@ -7,31 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
 @SpringBootApplication
 public class TimeComplexityAnalyzerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeComplexityAnalyzerApplication.class, args);
-
-        Lexer lexer = new Lexer("// Outer loop (Rows)\n" +
-                "        for (int i = 1; i <= 3; j/=) {" +
-                "i/=" +
-                "\n" +
-                "            \n" +
-                "            // Inner loop (Columns)\n" +
-                "            for (int j = 1; j <= 3; j++) {\n" +
-                "                System.out.print(\"(\" + i + \", \" + j + \") \");\n" +
-                "            }\n" +
-                "            \n" +
-                "            // Moves to the next line after the inner loop finishes a row\n" +
-                "            System.out.println(); \n" +
-                "        }");
-        lexer.scan();
-        lexer.printTokens();
-        Parser parser = new Parser(lexer);
-        var t = parser.parse();
-        parser.walk(t);
-
     }
 }
